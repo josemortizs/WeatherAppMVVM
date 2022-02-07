e4eec0ebafbc26ae77b8f255a6db155803061a1e
José Manuel Ortiz Sánchez <josemortizs@gmail.com>
2022-02-07 20:48:18 +0100

    Initial Commit

---
+++ WeatherAppMVVM/WeatherAppMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppMVVMApp.swift
+//  WeatherAppMVVM
+//
+//  Created by Jose Manuel Ortiz Sanchez on 7/2/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
